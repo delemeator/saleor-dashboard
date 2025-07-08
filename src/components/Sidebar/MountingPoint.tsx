6ab1d82bd1697fefd964db7e1eb3f70426f4db19
Pawel Romanski <pawel.romanski@dajar.pl>
2025-07-08 07:39:17 +0200

    allow popups

--- src/components/Sidebar/MountingPoint.tsx
+++ src/components/Sidebar/MountingPoint.tsx
@@ -1,6 +1,7 @@
 import sideBarDefaultLogoDarkMode from "@assets/images/sidebar-deafult-logo-darkMode.png";
 import sideBarDefaultLogo from "@assets/images/sidebar-default-logo.png";
 import { useLegacyThemeHandler } from "@dashboard/components/Sidebar/user/Controls";
+import useShop from "@dashboard/hooks/useShop";
 import { Avatar, Box, Text } from "@saleor/macaw-ui-next";
 import React from "react";
 
@@ -8,11 +9,13 @@ export const MountingPoint = () => {
   const { theme } = useLegacyThemeHandler();
   const logo = theme === "defaultLight" ? sideBarDefaultLogo : sideBarDefaultLogoDarkMode;
 
+  const shop = useShop();
+
   return (
     <Box display="flex" gap={3} paddingX={4} paddingY={5} alignItems="center">
       <Avatar.Store src={logo} scheme="accent1" size="small" />
       <Text size={3} fontWeight="bold">
-        Saleor Dashboard
+        {shop?.name || ""}
       </Text>
     </Box>
   );
