640d6485462e25f40f1ebf1f5a48474e7d197b55
Pawel Romanski <pawel.romanski@dajar.pl>
2026-03-18 15:40:10 +0100

    disable delete

--- src/categories/components/CategorySubcategories/CategorySubcategories.tsx
+++ src/categories/components/CategorySubcategories/CategorySubcategories.tsx
@@ -2,12 +2,14 @@ import { categoryAddUrl } from "@dashboard/categories/urls";
 import { BulkDeleteButton } from "@dashboard/components/BulkDeleteButton";
 import { DashboardCard } from "@dashboard/components/Card";
 import { InternalLink } from "@dashboard/components/InternalLink";
-import { CategoryDetailsQuery } from "@dashboard/graphql";
+import { CategoryDetailsQuery, PermissionEnum } from "@dashboard/graphql";
 import { ListProps, ListViews, RelayToFlat } from "@dashboard/types";
 import { Box, Button } from "@saleor/macaw-ui-next";
 import { FormattedMessage } from "react-intl";
 
 import { CategoryListDatagrid } from "../CategoryListDatagrid";
+import { useUser } from "@dashboard/auth";
+import { hasPermission } from "@dashboard/auth/misc";
 
 interface CategorySubcategoriesProps
   extends Pick<ListProps<ListViews.CATEGORY_LIST>, "onUpdateListSettings" | "settings"> {
@@ -27,6 +29,10 @@ export const CategorySubcategories = ({
   settings,
   onUpdateListSettings,
 }: CategorySubcategoriesProps) => {
+
+  const {user} = useUser();
+  const canEdit = user && hasPermission(PermissionEnum.MANAGE_MENUS, user);
+
   return (
     <DashboardCard>
       <DashboardCard.Header>
@@ -58,7 +64,7 @@ export const CategorySubcategories = ({
         onSelectCategoriesIds={onSelectCategoriesIds}
         selectionActionButton={
           <Box paddingRight={5}>
-            <BulkDeleteButton onClick={onCategoriesDelete}>
+            <BulkDeleteButton onClick={onCategoriesDelete} disabled={!canEdit}>
               <FormattedMessage defaultMessage="Delete categories" id="FiO/W/" />
             </BulkDeleteButton>
           </Box>
